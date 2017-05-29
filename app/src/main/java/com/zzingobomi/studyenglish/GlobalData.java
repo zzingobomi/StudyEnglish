5fd48486685585803ad6f3f2ff9495ab8661d28f
JONGCHAN <zzingo5@naver.com>
2017-05-29 13:27:13 +0900

    sound

--- app/src/main/java/com/zzingobomi/studyenglish/GlobalData.java
+++ app/src/main/java/com/zzingobomi/studyenglish/GlobalData.java
@@ -6,6 +6,10 @@ package com.zzingobomi.studyenglish;
 
 public class GlobalData
 {
+
+
+
+
     ///
     /// Debug 관련
     ///
@@ -14,6 +18,11 @@ public class GlobalData
     ///
     /// DB 관련
     ///
+    public static final String szMidAngelTableName  = "midangel";           // 미드 천사
+    //public static final String szTableName        = "midangel";           // 영화 영작
+    //public static final String szTableName        = "midangel";           // 불규칙 동사
+    //public static final String szTableName        = "midangel";           // 1004 어휘
+
     public static final int iIndexColumn            = 0;
     public static final int iKorSentenceColumn      = 1;
     public static final int iEngSentenceColumn      = 2;
